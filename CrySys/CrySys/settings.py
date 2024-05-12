8824dd483ed873a254a7368efcb65bd10c69231d
jeetpatel313 <jp2398d@gmail.com>
2024-05-12 16:22:48 -0700

    changed topics

--- CrySys/CrySys/settings.py
+++ CrySys/CrySys/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hi!kqm+b8%hpykaz38zkb!onf=r%m)c@&e@5-@i(o_75-n^16m'
+# SECRET_KEY = none
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
